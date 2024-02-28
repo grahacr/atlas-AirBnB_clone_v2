c505eb1b892d3671d6ad7b2ea2c1a377538d9dbc
grahacr <courtney.graham@atlasschool.com>
2024-02-28 11:17:04 -0600

    pycodestyle

--- models/city.py
+++ models/city.py
@@ -4,10 +4,11 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, base
 
-class City(BaseModel, base): 
+
+class City(BaseModel, base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    id = Column(String(60), primary_key=True, nullable=False) 
+    id = Column(String(60), primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", backref="city", cascade="all, delete")
--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, base
 from sqlalchemy import Table, Column, Integer, String, ForeignKey
 
+
 class State(BaseModel, base):
     """ State class """
     __tablename__ = 'states'
