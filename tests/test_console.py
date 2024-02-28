412e5d552f8d83d60e5df8102fb07d9c78e07a77
grahacr <courtney.graham@atlasschool.com>
2024-02-28 11:39:49 -0600

    add console test file

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" """
+import os
+import models
+import unittest
+import datetime
+from uuid import UUID
+import json
+from models.base_model import BaseModel
+
+
+class test_basemodel(unittest.TestCase):
