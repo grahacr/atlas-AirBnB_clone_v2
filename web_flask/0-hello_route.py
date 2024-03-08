7ac7dd0b7460df18700d084fc2c1091be3034a20
grahacr <courtney.graham@atlasschool.com>
2024-03-08 12:42:04 -0600

    forgot @ sign for route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def home():
     """function for returning script when home is visited"""
     return "Hello HBNB!"
