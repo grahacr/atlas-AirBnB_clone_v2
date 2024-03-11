a34f0149233963568a8a8f141ace9bedb8b5287e
grahacr <courtney.graham@atlasschool.com>
2024-03-11 12:16:48 -0500

    two routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module for starting Flask app"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """function for returning script when root is visited"""
+    return "Hello HBNB!"
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """function for returning script when hbnb is visited"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
