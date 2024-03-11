58c5cf7ad237b2812b27ac5309b7c10bf00259eb
grahacr <courtney.graham@atlasschool.com>
2024-03-11 12:57:18 -0500

    convert argument to integer and route to number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""module for starting Flask app with 3 routes"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """function for returning script when root is visited"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """function for returning script when hbnb is visited"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """function for taking in text as part of route"""
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text="is cool"):
+    """display python text is cool"""
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
