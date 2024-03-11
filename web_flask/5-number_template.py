4e4136f87b7940195fe5bfcbab79b56ae596eafd
grahacr <courtney.graham@atlasschool.com>
2024-03-11 13:43:34 -0500

    pep8

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,13 +37,15 @@ def display_python(text="is cool"):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
-    """route with number"""
+    """function for route with number"""
     return f"{n} is a number"
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_html(n):
+    """render html template with integer"""
     return render_template('5-number.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
