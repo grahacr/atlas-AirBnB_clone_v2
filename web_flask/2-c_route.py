7a55b02eef90344e3060685fe1ccc5ce3789716c
grahacr <courtney.graham@atlasschool.com>
2024-03-11 12:30:44 -0500

    display text passed in with another route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""module for starting Flask app with 3 routes"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """function for returning script when root is visited"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """function for returning script when hbnb is visited"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
