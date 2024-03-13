77406fbef328fb81293a67d6a5b83e0044930b95
grahacr <courtney.graham@atlasschool.com>
2024-03-13 09:40:10 -0500

    undo city and state changes

--- models/city.py
+++ models/city.py
@@ -3,15 +3,12 @@
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, base
-import os
+
 
 class City(BaseModel, base):
     """ The city class, contains state ID and name """
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
-    else:
-        state_id = ""
-        name = ""
+    __tablename__ = 'cities'
+    id = Column(String(60), primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="city", cascade="all, delete")
--- models/state.py
+++ models/state.py
@@ -5,21 +5,19 @@ from models.city import City
 from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
 
 
 class State(BaseModel, base):
     """Representation of state """
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        @property
-        def cities(self):
-            import models
-            city_objs = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    city_objs.append(city)
-                    return city_objs
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    @property
+    def cities(self):
+        import models
+        city_objs = []
+        for city in models.storage.all("City").values():
+            if city.state_id == self.id:
+                city_objs.append(city)
+        return city_objs
